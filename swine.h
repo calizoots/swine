dbbd894bd675ddbaa4d68e40495d2bb7c45e3128
cali zoots <calizoots@gmail.com>
2025-08-09 06:36:06 +0100

    didnt work first time

--- swine.h
+++ swine.h
@@ -938,7 +938,9 @@ static inline FindDiscoveryRes SwineGetFiles() {
                 return fs::path(mod.entryPoint).parent_path().stem() == parentStemStr;
             });
 
-            if (entry.path().extension() == config.projType && !skip) {
+            auto ext = entry.path().extension();
+
+            if (ext == config.projType && startsWith(ext.string(), "._") && !skip) {
                 sources.push_back(entry.path().string());
             }
         }
